8721753dd7c9c425c16119d9ba8b163d86d71b8b
sayanazm <kkit1303@gmail.com>
2024-04-01 09:08:13 +0300

    test

--- app/Core/App.php
+++ app/Core/App.php
@@ -48,45 +48,7 @@ class App
 
     public function run(): void
     {
-        $requestUri = $_SERVER['REQUEST_URI'];
 
-        try {
-            if (isset($this->routes[$requestUri])) {
-                $requestMethod = $_SERVER['REQUEST_METHOD'];
-                $routeMethod = $this->routes[$requestUri];
-
-                if (isset($routeMethod[$requestMethod])) {
-                    $handler = $routeMethod[$requestMethod];
-
-                    $class = $handler['class'];
-                    $method = $handler['method'];
-                    $requestClass = $handler['requestClass'];
-
-                    $obj = $this->container->get($class);
-
-                    $request = new $requestClass($method, $requestUri, headers_list(), $_POST);
-                    $obj->$method($request);
-
-                } else {
-                    echo "Метод $requestMethod не поддерживается для $requestUri";
-                }
-            } else {
-                require_once './../View/not_found.html';
-            }
-        } catch (\Throwable $exception) {
-
-            $logger = $this->container->get(LoggerInterface::class);
-
-            $data = [
-                'message' => 'message: ' . $exception->getMessage(),
-                'file' => 'file: ' . $exception->getFile(),
-                'line' => 'line: ' . $exception->getLine(),
-            ];
-
-            $logger->error('The server cannot process the request: ', $data);
-
-            require_once '../View/500.html';
-        }
 
     }
 
